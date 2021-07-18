62ad9e535a035edfcfea4ddcd732cf1427f155f2
Hugh Garsden <hughgarsden@yahoo.com>
2021-07-18 19:53:00 +0200

    Add multiprocessing

--- run_all.py
+++ run_all.py
@@ -8,7 +8,7 @@ with open("config.yaml") as yfile:
 
 ON_ILIFU = os.path.isfile("/opt/slurm/bin/sbatch")
 
-def gen_batch_header(use_gpu=False):
+def gen_batch_header(use_gpu=False, nproc=1):
     
     if config["short_run"]["enable"]:
         time = config["short_run"]["gpu_run_time"] if use_gpu else config["short_run"]["run_time"]
@@ -17,7 +17,7 @@ def gen_batch_header(use_gpu=False):
         time = config["gpu_run_time"] if use_gpu else config["run_time"]
         mem = config["mem"]
 
-    cpu = 1
+    cpu = nproc     # Need to fix this all up for apocrita
     if use_gpu and not ON_ILIFU:
        mem /= 8
        cpu = 8
@@ -25,7 +25,7 @@ def gen_batch_header(use_gpu=False):
     if ON_ILIFU:
         batch_header="""#!/bin/bash
 #SBATCH --job-name='scaling'
-#SBATCH --ntasks=1
+#SBATCH --ntasks="""+str(nproc)+"""
 #SBATCH --cpus-per-task=1
 #SBATCH --mem="""+str(mem)+"""G"""+"""
 #SBATCH --output=scaling-%j.log
@@ -51,8 +51,14 @@ def gen_batch_header(use_gpu=False):
     return batch_header
 
 def run_scaling(which, param, use_gpu=False):
+    if "nproc" in config[which]:
+        nproc = config[which]["nproc"]
+    else:
+        nproc = 1
+
     f = open("scalings.sh", "w")
-    f.write(gen_batch_header(use_gpu)+"\n\n")
+    f.write(gen_batch_header(use_gpu, nproc)+"\n\n")
+    f.write("echo Batch group id: "+str(os.getpid())+"\n")
     f.write("echo Running "+which+" "+param+"\n")
     f.write("date"+"\n")
     f.write(config["pinterp"]+" scalings.py "+str(which)+" "+str(param)+"\n")
@@ -61,20 +67,20 @@ def run_scaling(which, param, use_gpu=False):
 
     if config["run_as_batch"]:
         if ON_ILIFU:
-            os.system("sbatch scalings.sh | tee -a jobs.txt")
+            os.system("sbatch scalings.sh | tee -a group_"+str(os.getpid())+".txt")
         else:
-            os.system("qsub scalings.sh  | tee -a jobs.txt")
+            os.system("qsub scalings.sh  | tee -a group_"+str(os.getpid())+".txt")
     else:
         os.system("nice sh scalings.sh")
 
-if os.path.exists("jobs.txt"):
-    os.remove("jobs.txt")
+if os.path.exists("group_"+str(os.getpid())+".txt"):
+    os.remove("group_"+str(os.getpid())+".txt")
 for p in [ "antennas", "channels", "times", "sources" ]:
-    run_scaling("hera_sim_cpu_az", p)
-    run_scaling("hera_sim_cpu_az_update", p)          
-    run_scaling("hera_sim_cpu_astropy", p)
-    run_scaling("hera_sim_cpu_beam_pix", p)
+    #run_scaling("hera_sim_cpu_az", p)
+    #run_scaling("hera_sim_cpu_az_update", p)          
+    #run_scaling("hera_sim_cpu_astropy", p)
+    #run_scaling("hera_sim_cpu_beam_pix", p)
     #run_scaling("hera_sim_gpu", p, True)
     #run_scaling("healvis", p)
-    #run_scaling("pyuvsim", p)
+    run_scaling("pyuvsim", p)
 
